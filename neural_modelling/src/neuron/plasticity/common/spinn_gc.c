070ec84cf764f3dc10ae65b901d3d46b2fd8309b
Mantas Mikaitis <mantas.mikaitis@student.cs.man.ac.uk>
2015-12-22 16:54:48 +0000

    Add spinn_gc.h and spinn_gc.c

---
+++ neural_modelling/src/neuron/plasticity/common/spinn_gc.c
@@ -0,0 +1,220 @@
+//------------------------------------------------------------------------------
+//
+// spinn_gc.c       Main SpiNNaker garbage collection implementation
+//
+// Copyright (C)    The University of Manchester - 2015-2016
+//
+// Author           Mantas Mikaitis
+//
+//------------------------------------------------------------------------------
+
+#include <sark.h>
+#include <stdio.h>
+
+#include "spin1_api.h"
+#include <debug.h>
+
+#include "./post_events.h"
+
+//------------------------------------------------------------------------------
+// Various debugging routines mainly to print out memory contents or
+// certain DTCM heap structure pointer values.
+//------------------------------------------------------------------------------
+
+
+/*
+
+Print out the memory contents of the given addresses in hex and ASCII.
+Each line contains 16 bytes of memory.
+
+*/
+void spinn_print_dtcm_heap (char *start, char *end) {
+
+  io_printf(IO_BUF, "\nPrinting memory. Start: %x End %x \n", start, ++end);
+
+  while (start <= end) {
+    io_printf (IO_BUF, "%x: ", start);
+
+    int n = 0;
+
+    // Loop through each block of next 16 bytes and print
+    // them out in HEX.
+    while (n < 16) {
+      io_printf(IO_BUF, "%02x ", *(start+n));
+      n++;
+    }
+
+    // Loop through each block of next 16 bytes and print
+    // them out in ASCII.
+    n = 0;
+    io_printf(IO_BUF, "  ");
+    while (n < 16) {
+      if (*(start+n) < 0x80 && *(start+n) > 0x1F)
+        io_printf (IO_BUF, "%2c", *(start+n));
+      else
+        io_printf (IO_BUF, ".");
+      n++;
+    }
+
+    // Point to next block
+    start += 16;
+    io_printf (IO_BUF, "\n");
+  }
+
+}
+
+
+/*
+Print free block regions that can be found on DTCM heap.
+*/
+void print_all_free_DTCM_heap_blocks() {
+
+    block_t *next_free = sark.heap -> free;
+
+    io_printf(IO_BUF, "\nThese are the free blocks on DTCM heap: \n");
+
+    while (next_free != NULL) {
+        io_printf(IO_BUF, "%x -- %x \n", next_free, next_free -> next);
+        next_free = next_free -> free;
+    }
+
+}
+
+
+/*
+
+Return the size of a block on DTCM heap in bytes.
+
+NOTE: Size includes additional size of a block_t structure that is attached
+to each object on the heap.
+
+*/
+uint sizeof_dtcm_block(block_t * pointer) {
+
+  // Read block_t structu of the given block
+  pointer -= 1;
+
+  // Calculate size in bytes
+  return ((pointer -> next) - pointer) * 8;
+
+}
+
+
+//------------------------------------------------------------------------------
+
+
+/*
+
+Using ARM Block Copy instructions, LDM/STM, copy given memory block
+16 bytes at a time.
+
+If specified block size is not a multiple of 16 the remaining
+number of bytes at the end are copied single byte at once.
+
+Inputs: dest, src : Destination and source of the copying.
+        n         : Size of the objects in bytes.
+
+*/
+uint sark_block_copy(void *dest, const void *src, uint n) {
+
+  asm (
+    "      PUSH   {r0-r3}        \n\t"
+    "      LDR    r0, %[from]    \n\t"
+    "      LDR    r1, %[to]      \n\t"
+    "      MOV    r2, %[size]    \n\t"
+
+    "blockcopy:                  \n\t"
+    "      MOVS   r3, r2, LSR #4 \n\t"
+    "      BEQ    copybytes      \n\t"
+    "      PUSH   {r4-r7}        \n\t"
+
+    "quadcopy:                   \n\t"
+    "      LDM    r0!, {r4-r7}   \n\t"
+    "      STM    r1!, {r4-r7}   \n\t"
+    "      SUBS   r3, #1         \n\t"
+    "      BNE    quadcopy       \n\t"
+    "      POP    {r4-r7}        \n\t"
+
+    "copybytes:                  \n\t"
+    "      ANDS   r2, r2, #15    \n\t"
+    "      BEQ    stop           \n\t"
+    "bytecopy:                   \n\t"
+    "      LDRB   r3, [r0], #1   \n\t"
+    "      STRB   r3, [r1], #1   \n\t"
+    "      SUBS   r2, r2, #1     \n\t"
+    "      BNE    bytecopy       \n\t"
+
+    "stop:                       \n\t"
+    "      POP    {r0-r3}        \n\t"
+    :: [to] "m" (dest),      /* Address to copy block to */
+       [from] "m" (src),     /* Address where block resides */
+       [size] "r" (n)        /* Size in words */
+  );
+
+}
+
+
+/*
+
+Given a structure of history traces and a vector of live objects of each neuron
+copy all objects to sdram into  a single continuous block. Create a new vector
+of live objects which will store the indices to each history trace block as they
+appear in a compacted block.
+
+Returns a pointer to a new vector_t.
+
+*/
+uint copy_live_objects_to_sdram(void *traces, vector_t *live_objects, void *dest) {
+
+  int i;
+  int address_in_sdram = *dest;
+
+  // Create a working vector of live objects which eventually will be
+  // interchanged with the given live_objects.
+  vector_t *live_objects_changed =          spin1_malloc(sizeof(vector_t));
+  live_objects_changed -> object_indices =  spin1_malloc(n_neurons * sizeof(uint32_t));
+  live_objects_changed -> objects_sizes =   spin1_malloc(n_neurons * sizeof(uint32_t));
+
+  for (i = 0; i < sizeof(live_objects -> object_indices); i++) {
+    // Copy only those neuron traces that have an entry in the vector
+    // of live objects.
+    if ((live_objects -> object_indices)[i] >= 0) {
+      sark_block_copy(address_in_sdram,
+                      (int)traces + (live_objects -> object_indices)[i],
+                      (live_objects -> object_sizes)[i]);
+
+      (live_objects_changed -> object_indices)[i] =
+        (int) - address_in_sdram;
+      address_in_sdram += (live_objects -> object_sizes)[i];
+
+    }
+  }
+
+  return live_objects_changed;
+}
+
+
+/*
+
+Copy block of memory from SDRAM to a given data structure.
+
+*/
+void dma_compact_live_objects(int *src, int *dest, int size) {
+    int i;
+
+    log_info("DMA fired off.");
+
+    spin1_dma_transfer(0,
+                       *src,
+                       *dest,
+                       DMA_READ,
+                       size);
+}
+
+
+//------------------------------------------------------------------------------
+
+void c_main() {
+}
+
+//------------------------------------------------------------------------------
---
+++ neural_modelling/src/neuron/plasticity/common/spinn_gc.h
@@ -0,0 +1,98 @@
+//------------------------------------------------------------------------------
+//
+// spinn_gc.h       Header file for SpiNNaker garbage collector structures
+//
+// Copyright (C)    The University of Manchester - 2015-2016
+//
+// Author           Mantas Mikaitis
+//
+//------------------------------------------------------------------------------
+
+#include "./post_events.h"
+
+/*!
+
+We store references to live objects as indices in relation to the
+start of a some particular data structure address.
+
+E.g. if object is stored at location 0x400008 and it is part of
+array with a top address 0x400000 then the object entry in this
+vector will be 0x8, i.e. a size of all objects before it, in bytes.
+
+*/
+typedef struct {
+  uint32_t *object_indices;          // Pointer - START_OF_DTCM_HEAP
+  uint32_t *object_sizes;            // Size of objects, in bytes.
+  uint32_t size;                     // Size of all objects in bytes.
+} vector_t;
+
+
+//------------------------------------------------------------------------------
+// Useful debug functions
+
+
+/*
+
+Print out the memory contents of the given addresses in hex and ASCII.
+Each line contains 16 bytes of memory.
+
+*/
+
+void spinn_print_dtcm_heap (char *start, char *end);
+
+/*
+
+Print free block regions that can be found on DTCM heap.
+
+*/
+void print_all_free_DTCM_heap_blocks();
+
+/*
+
+Return the size of a block on DTCM heap in bytes.
+
+NOTE: Size includes additional size of a block_t structure that is attached
+to each object on the heap.
+
+*/
+uint sizeof_dtcm_block(block_t * pointer);
+
+
+//------------------------------------------------------------------------------
+// Garbage collection routines
+
+/*
+
+Using ARM Block Copy instructions, LDM/STM, copy given memory block
+16 bytes at a time.
+
+If specified block size is not a multiple of 16 the remaining
+number of bytes at the end are copied single byte at once.
+
+Inputs: dest, src : Destination and source of the copying.
+        n         : Size of the objects in bytes.
+
+*/
+uint sark_block_copy(void *dest, const void *src, uint n);
+
+/*
+
+Given a structure of history traces and a vector of live objects of each neuron
+copy all objects to sdram into  a single continuous block. Create a new vector
+of live objects which will store the indices to each history trace block as they
+appear in a compacted block.
+
+Returns a pointer to a new vector_t.
+
+*/
+uint copy_live_objects_to_sdram(void *traces, vector_t *live_objects, int *dest);
+
+
+/*
+
+Copy block of memory from SDRAM to a given data structure.
+
+*/
+void dma_compact_live_objects(int *src, int *dest, int size);
+
+//------------------------------------------------------------------------------
